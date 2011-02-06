d675bf3a78ff5a68b33e0f5de3fa7f012910dcbf
Raziel Alvarez <raziel.alvarez@appian.com>
2011-02-06 13:08:07 -0500

    Mvn: Added pom file, plus dummy test.

---
+++ src/test/java/com/raz/db/ConnectionPoolTest.java
@@ -0,0 +1,13 @@
+package com.raz.db;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConnectionPoolTest {
+
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
