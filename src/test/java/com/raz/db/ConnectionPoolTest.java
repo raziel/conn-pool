df3d68359e2a4ba95d85100fa82b97344d01fa98
Raziel Alvarez <raziel.alvarez@appian.com>
2011-02-12 08:08:24 -0500

    Conn: Commented out rountrip tests using H2 DB to avoid compilation issues since the H2 jar is not included yet (because it's really not used for tests yet)

--- src/test/java/com/raz/db/ConnectionPoolTest.java
+++ src/test/java/com/raz/db/ConnectionPoolTest.java
@@ -1,24 +1,7 @@
 package com.raz.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import org.junit.Ignore;
 
-import junit.framework.Assert;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.h2.jdbcx.JdbcDataSource;
-
-import sun.jdbc.odbc.JdbcOdbcDriver;
-
-import com.raz.db.conn.DSConnectionProvider;
-import com.raz.db.conn.DriverManagerConnectionProvider;
-import com.raz.db.conn.PDSConnectionProvider;
-import com.raz.db.conn.PooledConnectionWrapper;
-import com.raz.db.conn.SimpleConnectionWrapper;
-import com.raz.db.mng.SimpleConnectionManager;
 
 /**
  * Sample round-trip tests.
@@ -27,71 +10,73 @@ import com.raz.db.mng.SimpleConnectionManager;
  * @author raziel.alvarez
  *
  */
+@Ignore
 public class ConnectionPoolTest {
 
-  protected final Log logger = LogFactory.getLog(getClass());
 
-//  @Test
-  public void testDataStore() throws Exception {
-    JdbcDataSource dataSource = new JdbcDataSource();
-    dataSource.setURL("jdbc:h2:test");
-
-    Connection connection = null;
-    PreparedStatement statement = null;
-    try {
-      RegularCappedConnectionPool dspool = new RegularCappedConnectionPool(new SimpleConnectionManager<SimpleConnectionWrapper>(new DSConnectionProvider(dataSource)), 10, 100);
-      connection = dspool.getConnection();
-      final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
-      statement = connection.prepareStatement(sql);
-      ResultSet rs = statement.executeQuery();
-      if (!rs.next()) throw new Exception("Failure");
-      Assert.assertEquals("TEST", rs.getString(1));
-    } finally {
-      if (statement != null) { statement.close(); }
-      if (connection != null) { connection.close(); }
-    }
-  }
-
-//  @Test
-  public void testPooledDataStore() throws Exception {
-    JdbcDataSource dataSource = new JdbcDataSource();
-    dataSource.setURL("jdbc:h2:test");
-
-    Connection connection = null;
-    PreparedStatement statement = null;
-    try {
-      PooledCappedConnectionPool pdspool = new PooledCappedConnectionPool(new SimpleConnectionManager<PooledConnectionWrapper>(new PDSConnectionProvider(dataSource)), 10, 100);
-      connection = pdspool.getConnection();
-      final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
-      statement = connection.prepareStatement(sql);
-      ResultSet rs = statement.executeQuery();
-      if (!rs.next()) throw new Exception("Failure");
-      Assert.assertEquals("TEST", rs.getString(1));
-    } finally {
-      if (statement != null) { statement.close(); }
-      if (connection != null) { connection.close(); }
-    }
-  }
-
-//  @Test
-  public void testDriverManager() throws Exception {
-    JdbcOdbcDriver driver = new JdbcOdbcDriver();
-    DriverManager.registerDriver(driver);
-
-    Connection connection = null;
-    PreparedStatement statement = null;
-    try {
-      RegularCappedConnectionPool dmpool = new RegularCappedConnectionPool(new SimpleConnectionManager<SimpleConnectionWrapper>(new DriverManagerConnectionProvider("jdbc:h2:test")), 10, 100);
-      connection = dmpool.getConnection();
-      final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
-      statement = connection.prepareStatement(sql);
-      ResultSet rs = statement.executeQuery();
-      if (!rs.next()) throw new Exception("Failure");
-      Assert.assertEquals("TEST", rs.getString(1));
-    } finally {
-      if (statement != null) { statement.close(); }
-      if (connection != null) { connection.close(); }
-    }
-  }
+////@Test
+//public void testDataStore() throws Exception {
+//JdbcDataSource dataSource = new JdbcDataSource();
+//dataSource.setURL("jdbc:h2:test");
+//
+//ConnectionPool dspool = null;
+//Connection connection = null;
+//PreparedStatement statement = null;
+//try {
+//  dspool = new RegularCappedConnectionPool(new SimpleConnectionManager<SimpleConnectionWrapper>(new DSConnectionProvider(dataSource)), 10, 100);
+//  connection = dspool.getConnection();
+//  final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
+//  statement = connection.prepareStatement(sql);
+//  ResultSet rs = statement.executeQuery();
+//  if (!rs.next()) throw new Exception("Failure");
+//  Assert.assertEquals("TEST", rs.getString(1));
+//} finally {
+//  if (statement != null) { statement.close(); }
+//  if (connection != null) { dspool.releaseConnection(connection); }
+//}
+//}
+//
+////@Test
+//public void testPooledDataStore() throws Exception {
+//JdbcDataSource dataSource = new JdbcDataSource();
+//dataSource.setURL("jdbc:h2:test");
+//
+//Connection connection = null;
+//PreparedStatement statement = null;
+//try {
+//  ConnectionPool pdspool = new PooledCappedConnectionPool(new SimpleConnectionManager<PooledConnectionWrapper>(new PDSConnectionProvider(dataSource)), 10, 100);
+//  connection = pdspool.getConnection();
+//  final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
+//  statement = connection.prepareStatement(sql);
+//  ResultSet rs = statement.executeQuery();
+//  if (!rs.next()) throw new Exception("Failure");
+//  Assert.assertEquals("TEST", rs.getString(1));
+//} finally {
+//  if (statement != null) { statement.close(); }
+//  if (connection != null) { connection.close(); }
+//}
+//}
+//
+////@Test
+//public void testDriverManager() throws Exception {
+//JdbcOdbcDriver driver = new JdbcOdbcDriver();
+//DriverManager.registerDriver(driver);
+//
+//ConnectionPool dmpool = null;
+//Connection connection = null;
+//PreparedStatement statement = null;
+//try {
+//  dmpool = new RegularCappedConnectionPool(new SimpleConnectionManager<SimpleConnectionWrapper>(new DriverManagerConnectionProvider("jdbc:h2:test")), 10, 100);
+//  connection = dmpool.getConnection();
+//  final String sql = "SELECT * FROM INFORMATION_SCHEMA.CATALOGS;";
+//  statement = connection.prepareStatement(sql);
+//  ResultSet rs = statement.executeQuery();
+//  if (!rs.next()) throw new Exception("Failure");
+//  Assert.assertEquals("TEST", rs.getString(1));
+//} finally {
+//  if (statement != null) { statement.close(); }
+//  if (connection != null) { dmpool.releaseConnection(connection); }
+//}
+//}
 
 }
